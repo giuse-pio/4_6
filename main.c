1cf54db0b1d94bf1ee17a24297b4eb6d7ca6fb3f
giuse-pio <giupeppepio7@gmail.com>
2024-10-15 17:55:04 +0200

    fatto con il ciclo for

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-    float area, raggio, perimetro;
-    raggio = 1;
-    while (raggio<=20) {
-        perimetro = (2*3.14)*raggio;
-        printf("il perimetro e': %.2f \n", perimetro);
+    float area=0, raggio=0, circonferenza=0;
+    /*while (raggio<=20) {
+        circonferenza = (2*3.14)*raggio;
+        printf("il perimetro e': %.2f \n", circonferenza);
 
         area = raggio*raggio*(3.14);
         printf("l'area del cerchio e': %.2f \n", area);
 
         raggio++;
+    }*/
+    for (raggio = 1; raggio <= 20; raggio++) {
+        circonferenza = (2*3.14)*raggio;
+        printf("il perimetro e': %.2f || ", circonferenza);
+        area = raggio*raggio*(3.14);
+        printf("l'area del cerchio e': %.2f \n", area);
     }
-
 }
